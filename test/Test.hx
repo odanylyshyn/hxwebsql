3c8cb78c52a400d33704377e519f84cb7d19fdc5
Danylyshyn <danylyshyn@igra123.org>
2017-10-18 12:55:24 +0300

    update test script

--- test/Test.hx
+++ test/Test.hx
@@ -1,46 +1,62 @@
 package;
 
 import js.Browser;
+import js.html.Element;
 import js.sqlite.Database;
 import js.sqlite.Transaction;
 import js.sqlite.SqlQuery;
 import js.sqlite.SqlOperator;
 
 class Test {
+	static private var instance:Test;
+	private var db:Database;
 
 	static public function main():Void {
+		instance = new Test();
+    }  
 
-		// create or open database
-		var db = new Database("TestDB");
+    public function new():Void {
+    	db = new Database("TestDB");
+        getElem('nojs').style.display = 'none';
+        getElem('container').style.display = 'block';
+        getElem('btnCreateTable').addEventListener('click', createTable);
+        getElem('btnInsertTwo').addEventListener('click', insertTwoRows);
+        getElem('btnInsertMset').addEventListener('click', insertRowMset);
+    }
 
-		// create table
-		var tr1 = new Transaction(db);
-		tr1.addQuery( new SqlQuery('CREATE TABLE IF NOT EXISTS records (username, score)') );
-		tr1.exec();
+    inline private function getElem(id:String):Element {
+        return Browser.document.getElementById(id);
+    }
 
-		// add two rows
-		var tr2 = new Transaction(db);
+    private function createTable():Void {
+    	trace('run creating table');
+        var tr = new Transaction(db);
+		tr.addQuery( new SqlQuery('CREATE TABLE IF NOT EXISTS records (username, score)') );
+		tr.exec();
+    }
+
+    private function insertTwoRows():Void {
+    	var tr = new Transaction(db);
 		var q1 = new SqlQuery('records', SqlOperator.INSERT);
 		q1.set('username', 'Bob');
 		q1.set('score', 123);
-		tr2.addQuery(q1);
-
+		tr.addQuery(q1);
 		var q2 = new SqlQuery('records', SqlOperator.INSERT);
 		q2.set('username', 'Joe');
 		q2.set('score', 456);
-		tr2.addQuery(q2);
-		
-		tr2.exec();
+		tr.addQuery(q2);
+		tr.exec();
+    }
 
-		// using method "mset"
-		var tr3 = new Transaction(db);
-		var q3 = new SqlQuery('records', SqlOperator.INSERT);
-		q3.mset({username: 'Peter', score: 675});
-		tr3.addQuery( q3 );
-		tr3.exec();
+    private function insertRowMset():Void {
+        var tr = new Transaction(db);
+		var q = new SqlQuery('records', SqlOperator.INSERT);
+		q.mset({username: 'Peter', score: 675});
+		tr.addQuery(q);
+		tr.exec();
+    }
 
-
-    }  
     
+
 }
 
